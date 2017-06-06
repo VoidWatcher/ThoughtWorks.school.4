534d2bf3204aa1ebb5aa30be44a2b3f3bb4c6a5a
VoidWatcher <VoidWatcher@users.noreply.github.com>
2017-06-07 01:10:50 +0800

    2.2

--- main/student.js
+++ main/student.js
@@ -1,10 +1,12 @@
-let Person = require('./person')
+let Person = require('./person');
 // Write your code here
-var Student = function(name, age, class) {
+function Student(name, age, class){
+  this.name = name;
+  this.age = age;
   this.class = class;
   this.introduce = function() {
     console.log("My name is " + this.name + ". I am " + this.age + " years old. I am a Student. I am at Class " + this.class + ".");
   }  
 }
 Student.prototype = Person;
-module.exports = Student
+module.exports = Student;
