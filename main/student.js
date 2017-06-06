cc989240cef2164c28744ba0e6b1d1819be444ab
VoidWatcher <VoidWatcher@users.noreply.github.com>
2017-06-07 01:17:52 +0800

    3.1

--- main/student.js
+++ main/student.js
@@ -1,9 +1,9 @@
 let Person = require('./person');
 // Write your code here
-function Student(name, age, class){
+function Student(name, age, klass){
   this.name = name;
   this.age = age;
-  this.class = class;
+  this.class = klass;
   this.introduce = function() {
     console.log("My name is " + this.name + ". I am " + this.age + " years old. I am a Student. I am at Class " + this.class + ".");
   }  
