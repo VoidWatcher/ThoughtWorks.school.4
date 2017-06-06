4c9907ca43a1b2706b53991a892bd6a722c37b8c
VoidWatcher <VoidWatcher@users.noreply.github.com>
2017-06-07 02:22:01 +0800

    5.1

--- main/person.js
+++ main/person.js
@@ -1,9 +1,9 @@
 // Write your code here
-var Person = function(name, age){
+function Person(name, age){
   this.name = name;
   this.age = age;
   this.introduce = function() {
     console.log("My name is " + this.name + ". I am " + this.age + " years old.");
   }  
 }  
-module.exports = Person
+module.exports = Person;
