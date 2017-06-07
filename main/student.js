88fb451e9896c354f9b6409eabbc2bf2958614c7
VoidWatcher <VoidWatcher@users.noreply.github.com>
2017-06-07 16:28:14 +0800

    8.2

--- main/student.js
+++ main/student.js
@@ -1,10 +1,10 @@
 let Person = require('./person');
 // Write your code here
 function Student(name, age, klass){
-  Person.apply(this, arguments);
+  Person.call(this, name, age);
   this.class = klass;
   this.introduce = function() {
-    console.log("My name is " + this.name + ". I am " + this.age + " years old. I am a Student. I am at Class " + this.class + ".");
+    return "My name is " + this.name + ". I am " + this.age + " years old. I am a Student. I am at Class " + this.class + ".";
   }  
 }
 module.exports = Student;
